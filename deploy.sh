d3e2413e659ab8965a6564baaa81bd89ae5e2e07
zhanghui <zhangyihui_vendor@sensetime.com>
2021-05-14 16:27:41 +0800

    edit page

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 
-git push -f git@github.com:zhanghui08/zhanghui.github.io.git master
+git push -f git@github.com:zhanghui08/zhanghui.github.io.git master:gh-pages
 # 例如 git push -f git@github.com:koala-coding/blog.git master:gh-pages 
 cd -
